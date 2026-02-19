a030ed9828e3df9cd496bc455453d477db243bf9
Acid <Acid-aja@users.noreply.github.com>
2026-02-19 18:14:18 +0700

    version: change to java 21 from 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
-plugins {
-    java
-    id("org.springframework.boot") version "4.0.2"
-    id("io.spring.dependency-management") version "1.1.7"
-}
-
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "6.1.0"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.10"
+    id("io.spring.dependency-management") version "1.1.7"
+}
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
@@ -14,7 +14,7 @@ description = "eshop"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -29,21 +29,24 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
     testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
     testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter:${junitJupiterVersion}")
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+// Mendaftarkan task untuk Unit Test (mengecualikan FunctionalTest)
 tasks.register<Test>("unitTest") {
     description = "Runs unit tests."
     group = "verification"
@@ -53,6 +56,7 @@ tasks.register<Test>("unitTest") {
     }
 }
 
+// Mendaftarkan task untuk Functional Test (hanya menjalankan FunctionalTest)
 tasks.register<Test>("functionalTest") {
     description = "Runs functional tests."
     group = "verification"
@@ -62,6 +66,7 @@ tasks.register<Test>("functionalTest") {
     }
 }
 
+// Konfigurasi agar semua tes menggunakan JUnit Platform
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
